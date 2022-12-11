7ba6589a5a0fad7aca7c2772f4c3180d6808a998
Exilon <explunki@outlook.com>
2022-12-11 16:15:26 +0400

    Added files

---
+++ OnScreenText/Config.cs
@@ -0,0 +1,13 @@
+﻿namespace OnScreenText
+{
+    using Exiled.API.Interfaces;
+    
+    public class Config : IConfig
+    {
+        public bool IsEnabled { get; set; } = true;
+
+        public string Title { get; set; } = "MY SERVER";
+
+        public string Text { get; set; } = "Welcome to my server!";
+    }
+}
---
+++ OnScreenText/Plugin.cs
@@ -0,0 +1,24 @@
+﻿using Exiled.Events.EventArgs;
+
+namespace OnScreenText
+{
+    using Exiled.API.Features;
+    
+    public class Plugin : Plugin<Config>
+    {
+        public override void OnEnabled()
+        {
+            Exiled.Events.Handlers.Player.Verified += PlayerOnVerified;
+        }
+        
+        public override void OnDisabled()
+        {
+            Exiled.Events.Handlers.Player.Verified -= PlayerOnVerified; 
+        }
+
+        private void PlayerOnVerified(VerifiedEventArgs ev)
+        {
+            ev.Player.ShowHint($"\n\n\n\n\n\n\n\n\n\n\n\n\n{Config.Title}\n{Config.Text}", 2400f);
+        }
+    }
+}
